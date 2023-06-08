7aea4a6aa9ad1231ef746c43183df460a4afa7b0
Jonas <jonas.jiunchetti@outlook.com>
2023-06-08 16:15:31 -0300

    get all clients

--- src/main/java/com/challenge/Challenge3/services/ClientService.java
+++ src/main/java/com/challenge/Challenge3/services/ClientService.java
@@ -1,6 +1,8 @@
 package com.challenge.Challenge3.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.challenge.Challenge3.DTO.ClientDTO;
@@ -16,6 +18,11 @@ public class ClientService {
     public ClientDTO findById(Long id){
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 
 }
